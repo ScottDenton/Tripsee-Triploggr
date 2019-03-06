06f336b306cea6356af5f1c0494789f6a44c7863
ScottDenton <scottyd_14@hotmail.com>
2019-03-05 16:20:31 -0800

    Moving user files around

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :trips do
-    resources :memories
-  end
   
+  resources :users do
+    resources :trips do
+      resources :memories
+    end
+  end
+
   resources :comments
   resources :locations
-  resources :users
+
 
   root  'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
