31448145a624e3c6670289f0832e2533e9c2a166
ScottDenton <scottyd_14@hotmail.com>
2019-03-07 11:49:06 -0800

    Validations added and css to forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ApplicationRecord
   has_many :memories
   has_secure_password
 
-  
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true
+
+
+
 
 
 
