59a0735056d842a4898763a416e3190b447dd038
ScottDenton <scottyd_14@hotmail.com>
2019-03-08 09:26:27 -0800

    Trip new form fixed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,20 @@
 class LocationsController < ApplicationController
+  helper_method :location_already_created
+
   def create
+      @location = Location.new(location_params)
+      @user = User.find(session[:user_id])
+        if @location.city.downcase == params[:location][:city].downcase
+          flash[:notice] = "This city has already been added"
+          redirect_to new_user_trip_path(@user)
+        else
+          @location.save
+          redirect_to new_user_trip_path(@user)
+        end
+  end
 
-    @location = Location.create(location_params)
-    @user = User.find(session[:user_id])
-
-    redirect_to new_user_trip_path(@user)
+  def location_already_created
+   Location.all.find{|location| location.city == params[:city]}
   end
 
 
